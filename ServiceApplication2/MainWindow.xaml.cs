7628103aabadd6a5f615a065c6c515b194816fc5
Raihan Khalil Abdillah <tatuhey@gmail.com>
2023-05-03 17:06:29 +0800

    6.5 is kinda meh, 6.6 6.7 6.8 6.9 6.10 are kinda OK

--- ServiceApplication2/MainWindow.xaml.cs
+++ ServiceApplication2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ServiceApplication2
 {
@@ -47,8 +48,9 @@ namespace ServiceApplication2
                 string problem = tbServiceProblem.Text;
                 double cost = double.Parse(tbServiceCost.Text);
                 int tag = int.Parse(iuoServiceTag.Text);
+                int priority = GetServicePriority();
 
-                if (rbExpress.IsChecked == true)
+                if (priority == 1)
                 {
                     Drone newRegular = new Drone();
                     newRegular.setName(name);
@@ -59,13 +61,13 @@ namespace ServiceApplication2
                     RegularService.Enqueue(newRegular);
                 }
 
-                if (rbExpress.IsChecked == true) 
+                if (priority == 0)
                 {
                     Drone newExpress = new Drone();
                     newExpress.setName(name);
                     newExpress.setModel(model);
                     newExpress.setProblem(problem);
-                    newExpress.setCost(cost);
+                    newExpress.setCost(ExpressCost(cost));
                     newExpress.setTag(tag);
                     ExpressService.Enqueue(newExpress);
                 }
@@ -77,20 +79,56 @@ namespace ServiceApplication2
         }
 
         //6.6	Before a new service item is added to the Express Queue the service cost must be increased by 15%.
+        private double ExpressCost(double cost)
+        {
+            double expCost = cost * 15 / 100;
+            return expCost;
+        }
 
         //6.7	Create a custom method called “GetServicePriority” which returns the value of the priority radio group.
         //This method must be called inside the “AddNewItem” method before the new service item is added to a queue.
-        private bool GetServicePriority(GroupBox group, int index)
+        private int GetServicePriority()
         {
-
+            //int value = -1;
+            if (rbExpress.IsChecked == true)
+                return 0;
+            if (rbRegular.IsChecked == true)
+                return 1;
+            else return -1;
         }
+
         //6.8	Create a custom method that will display all the elements in the RegularService queue.
         //The display must use a List View and with appropriate column headers.
+        private void DisplayRegular()
+        {
+            lvRegular.Items.Clear();
+            lvRegular.ItemsSource = RegularService;
+        }
 
         //6.9	Create a custom method that will display all the elements in the ExpressService queue.
         //The display must use a List View and with appropriate column headers.
+        private void DisplayExpress()
+        {
+            lvExpress.Items.Clear();
+            lvExpress.ItemsSource = ExpressService;
+        }
 
         //6.10	Create a custom keypress method to ensure the Service Cost textbox can only accept a double value with one decimal point.
+        private void tbServiceCost_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //if (!char.IsDigit(e.Text, e.Text.Length - 1) && e.Text != "." || (e.Text == "." && ((TextBox)sender).Text.Contains(".")))
+            //{
+            //    e.Handled = true;
+            //}
+
+            TextBox textBox = (TextBox)sender;
+            string text = textBox.Text.Insert(textBox.CaretIndex, e.Text);
+
+            if (!char.IsDigit(e.Text, e.Text.Length - 1) && e.Text != "." || text.Count(c => c == '.') > 1)
+            {
+                e.Handled = true;
+            }
+        }
 
         //6.11	Create a custom method to increment the service tag control,
         //this method must be called inside the “AddNewItem” method before the new service item is added to a queue.
@@ -112,5 +150,6 @@ namespace ServiceApplication2
 
         //6.18	All code is required to be adequately commented. Map the programming criteria and features to your code/methods
         //by adding comments above the method signatures. Ensure your code is compliant with the CITEMS coding standards (refer http://www.citems.com.au/).
+
     }
 }
